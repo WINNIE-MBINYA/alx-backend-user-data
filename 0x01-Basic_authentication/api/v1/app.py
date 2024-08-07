5f0cb2dd96f891cca59a2dcaa58b8ea4a26f8d5f
WINNIE-MBINYA <winnymbinyamwanza@gmail.com>
2024-08-07 21:34:13 +0300

    Document code

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,32 +1,64 @@
 #!/usr/bin/env python3
 """
 Route module for the API
+This module sets up a Flask application with CORS support and defines
+error handlers for various HTTP status codes.
+
+Dependencies:
+- Flask: A web framework for Python.
+- Flask-CORS: A Flask extension for handling
+        Cross-Origin Resource Sharing (CORS).
+
+Usage:
+- Run the application by executing this script. The server will listen
+  on the host and port specified in the environment variables
+  API_HOST and API_PORT, or default to 0.0.0.0 and 5000, respectively.
 """
+
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
-import os
+from flask_cors import CORS
 
-
+# Create a Flask application instance
 app = Flask(__name__)
+
+# Register the app_views blueprint with the application
 app.register_blueprint(app_views)
+
+# Enable CORS for all routes under the /api/v1/ endpoint
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
+    """Error handler for 404 Not Found.
+
+    Args:
+        error: The error that occurred.
+
+    Returns:
+        A JSON response with an error message and a 404 status code.
     """
     return jsonify({"error": "Not found"}), 404
 
-@app.errohandler(401)
-def unauthorized(error) -> str: 
-    """Unauthorized handler
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """Error handler for 401 Unauthorized.
+
+    Args:
+        error: The error that occurred.
+
+    Returns:
+        A JSON response with an error message and a 401 status code.
     """
-    return jsonify({error: "Unauthorized"}), 401
+    return jsonify({"error": "Unauthorized"}), 401
+
 
 if __name__ == "__main__":
+    # Get the host and port from environment variables or use defaults
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
+    # Run the Flask application
     app.run(host=host, port=port)
