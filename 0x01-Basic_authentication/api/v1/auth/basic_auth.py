3e8793cbe4adcb098db78ed4ddad2952589b0caf
WINNIE-MBINYA <winnymbinyamwanza@gmail.com>
2024-08-08 19:10:19 +0300

    Create empty class

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module for Basic Authentication
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """BasicAuth class that inherits from Auth.
+    Currently, this class is empty and serves as a placeholder for future
+    basic authentication implementations.
+    """
+    pass
